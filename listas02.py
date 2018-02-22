9096e732d6a06206b47f45447f3b271260b8bae9
ism06659681 <ism06659681@a04.informatica.escoladeltreball.org>
2018-02-22 12:42:44 +0100

    lista 2.1

--- listas02.py
+++ listas02.py
@@ -1,13 +1,11 @@
 #coding: utf-8
 x = int(input("Diagme cuantas palabras tiene la lista: "))
-cont = 0
 while x<=0 :
 	x = int(input("¡Imposible! Pruebe otra vez: "))
-else : 
-	a = []
-	for i in range(x) :
-		palabra = raw_input("Digame la palabra "+str(i+1)+": ")
-		a += [palabra]
+a = []
+for i in range(x) :
+	palabra = raw_input("Digame la palabra "+str(i+1)+": ")
+	a += [palabra]
 print "La lista creada es",a
 pala = raw_input("Digame la palabra a buscar: ")
 print "La palabra",pala,"aparece",a.count(pala),"veces en la lista."
---
+++ listas03.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+x = int(input("Diagme cuantas palabras tiene la lista: "))
+cont = 0
+while x<=0 :
+	x = int(input("¡Imposible! Pruebe otra vez: "))
+a = []
+for 
